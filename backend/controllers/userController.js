891da4db90cdfd8993cbfc6a44d6bad19ce71e42
Jacob Cho <jacobcho@live.com>
2022-07-22 08:41:07 -0600

    async err handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,11 @@
+const asyncHandler = require('express-async-handler')
+//we are using async await, so we don't want to use try catch block
+//instead we handle errors in async express routes and pass them to the errorhandlers
 
 // Registers a new user
 // route: /api/users
 // access is public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler (async(req, res) => {
   const {name, email, password} = req.body
 
   //validation
@@ -12,14 +15,14 @@ const registerUser = (req, res) => {
   }
 
   res.send('Register Route')
-}
+})
 
 // Logs in user
 // route: /api/users/login
 // access is public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler (async(req, res) => {
   res.send('Login Route')
-}
+})
 
 module.exports = {
   registerUser,

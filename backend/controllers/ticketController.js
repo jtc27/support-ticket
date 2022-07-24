fb209a3958d6061e21b5cdaa28cbd8bb09af6cd2
Jacob Cho <jacobcho@live.com>
2022-07-24 17:09:45 -0600

    get a single ticket

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -17,9 +17,38 @@ const getTickets = asyncHandler (async(req, res) => {
   }
 
   const tickets = await Ticket.find({user: req.user.id})
-
   res.status(200).json(tickets)
 })
+
+// GET ONE USER TICKET
+// route: GET /api/tickets/:id
+// access is private
+const getTicket = asyncHandler (async(req, res) => {
+
+  //get user using the id from JWT
+  const user = await User.findById(req.user.id)
+
+  if(!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  //get the ticket ID from the URL
+  const ticket = await Ticket.findById(req.params.id)
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error('Ticket not found')
+  }
+
+  if(ticket.user.toString() !== req.user.id) {
+    res.status(404)
+    throw new Error('Not authorized') // Only the user can see their own tickets
+  }
+
+  res.status(200).json(ticket)
+})
+// ^ FOR THE TICKETS TO BE ACCESSED WE NEED A BEARER TOKEN and TICKET ID.  IF EITHER IS MISSING THERE WILL BE AN ERROR THROWN
 
 // CREATE A NEW TICKET
 // route: POST /api/tickets
@@ -55,5 +84,6 @@ const createTicket = asyncHandler (async(req, res) => {
 
 module.exports = {
   getTickets,
+  getTicket,
   createTicket,
 }
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const {getTickets, createTicket} = require('../controllers/ticketController')
+const {getTickets, getTicket, createTicket} = require('../controllers/ticketController')
 
 const {protect} = require('../middleware/authMiddleware')
 
+// in server.js WE HAVE: app.use('/api/tickets', require('./routes/ticketRoutes'))
 router.route('/').get(protect, getTickets).post(protect, createTicket)
+router.route('/:id').get(protect, getTicket)
 
 module.exports = router
